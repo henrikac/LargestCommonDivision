282dd24d692b82e1434a92731b326eaa5e35663f
Henrik Christensen <sensen1695@hotmail.com>
2018-10-08 12:42:26 +0200

    prompts user for two positive numbers

--- src/LargestCommonDivider.c
+++ src/LargestCommonDivider.c
@@ -8,9 +8,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum { false, true } bool;
+
 int main(void)
 {
-  
+  int num_one = 0, num_two = 0, highest_num = 0, lowest_num = 0;
+
+  /* prompts user for two integers */
+  while (true)
+  {
+    printf("Enter two positive numbers: ");
+    scanf(" %d %d", &num_one, &num_two);
+
+    /* checking if input is positive integers */
+    if (num_one > 0 && num_two > 0)
+    {
+      if (num_one > num_two)
+      {
+        highest_num = num_one;
+        lowest_num = num_two;
+      }
+      else
+      {
+        highest_num = num_two;
+        lowest_num = num_one;
+      }
+
+      break;
+    }
+
+    printf("\nPlease only enter positive numbers\n");
+  }
+
+  printf("Highest number = %d\nLowest number = %d", highest_num, lowest_num);
 
   return EXIT_SUCCESS;
 }
