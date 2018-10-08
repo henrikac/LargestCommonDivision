e8a1e93a41c884485d705a18013a35769c32b71f
Henrik Christensen <sensen1695@hotmail.com>
2018-10-08 07:46:06 +0200

    initial commit

---
+++ src/LargestCommonDivider.c
@@ -0,0 +1,16 @@
+/*
+ * Programmer: Henrik A. Christensen     Date Completed: in progress
+ * Instructor: Kurt Nørmark              Class:          Imperative Programming
+ *
+ * Coming soon...
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  
+
+  return EXIT_SUCCESS;
+}
